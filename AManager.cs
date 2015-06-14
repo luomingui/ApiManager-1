25a92d1a8942745eae84776d0ba8c180fbea1464
SethWilkenfeld <seth@wilkenfeld.net>
2015-06-14 00:18:19 -0400

    First iteration

---
+++ AManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ApiManager
+{
+    public class AManager
+    {
+        #region Constants & Enums
+        protected const string _MEDIA_TYPE = "application/";
+        public enum ResponseFormats { xml = 0, json = 1 }
+        #endregion
+
+        #region Protected Properties
+        protected readonly Uri _DefaultBaseUri;
+        protected HttpClient _ApiClient;
+        #endregion
+
+        public AManager(Uri defaultBaseUri)
+        {
+            _DefaultBaseUri = defaultBaseUri;
+            _ApiClient = new HttpClient(new WebRequestHandler { AllowAutoRedirect = false })
+                {
+                    BaseAddress = _DefaultBaseUri
+                };
+        }
+
+        /// <summary>
+        /// Sends an HttpRequest and returns the response string.
+        /// Handles redirect responses by updating the httpclient's header.location
+        /// </summary>
+        /// <param name="httpClient"></param>
+        /// <param name="requestString"></param>
+        /// <param name="responseFormat"></param>
+        /// <returns></returns>
+        public static async Task<string> GetResponseStringAsync(HttpClient httpClient, string requestString, ResponseFormats responseFormat)
+        {
+            bool resendRequest;
+            HttpResponseMessage responseMessage;
+            
+            do
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_MEDIA_TYPE + responseFormat));
+                
+                resendRequest = false;
+                responseMessage = httpClient.GetAsync(requestString).Result;
+                switch (responseMessage.StatusCode)
+                {
+                    case HttpStatusCode.Redirect:
+                        var newBaseUri = responseMessage.Headers.Location.ToString().Replace(requestString, string.Empty);
+                        httpClient = new HttpClient(new WebRequestHandler {AllowAutoRedirect = false})
+                            {
+                                BaseAddress = new Uri(newBaseUri)
+                            };
+                        resendRequest = true;
+                        break;
+                }
+            } while (resendRequest);
+
+            return await responseMessage.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ ARequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Specialized;
+using System.Web;
+
+namespace ApiManager
+{
+    public abstract class ARequest
+    {
+        protected const string _QUERY_PREFIX = @"?";
+        protected const string _DELIMETER = @"/";
+
+        public AManager.ResponseFormats ResponseFormat { get; set; }
+        public readonly NameValueCollection Query;
+
+        /// <summary>
+        /// Abstract class that stores the request data
+        /// </summary>
+        /// <param name="responseFormat"></param>
+        protected ARequest(AManager.ResponseFormats responseFormat)
+        {
+            ResponseFormat = responseFormat;
+            Query = HttpUtility.ParseQueryString(string.Empty);
+        }
+
+        /// <summary>
+        /// Clears the API request data
+        /// </summary>
+        public virtual void Clear() { Query.Clear(); }
+
+        /// <summary>
+        /// Returns a string representation of the API request
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() { return Query.ToString(); }
+    }
+}
